c8fdb1d6501a9d1e68a89c9664d5d750bd1ee293
Adithya <adithya.bellary@gmail.com>
2021-08-11 00:12:48 -0500

    fix dimensions

--- midas/investigation/lstm/run.py
+++ midas/investigation/lstm/run.py
@@ -8,8 +8,9 @@ NUM_FEATURES = 7
 NUM_HIDDEN_FEATURES = 10
 NUM_LAYERS = 2
 NUM_PREDICTIONS = 3
+NUM_ITEMS = 5
 
-inputs = [torch.randn(2, NUM_FEATURES).view(2,1,NUM_FEATURES) for _ in range(5)]
+inputs = [torch.randn(NUM_ITEMS, NUM_FEATURES).view(NUM_ITEMS,1,NUM_FEATURES) for _ in range(5)]
 
 class WordLSTM(torch_nn.Module):
   def __init__(self):
@@ -19,25 +20,31 @@ class WordLSTM(torch_nn.Module):
       NUM_HIDDEN_FEATURES,
       NUM_LAYERS,
       dropout=0.2,
-      # proj_size=NUM_PREDICTIONS
+      # if we want an output with dimensionality different than h
+      proj_size=NUM_PREDICTIONS
     )
 
   def forward(self, x):
-    print('x', x.size())
+    # print('x', x.size())
     out, h = self.lstm(x)
 
-    print(out)
-    print(h)
+    print('out in forward', out.size())
+    print('h in forward', len(h))
 
-    return out
+    # we want an output of dimension [1, NUM_PREDICTIONS]
+    out = torch.transpose(out.view(out.size()[0], out.size()[2]), 0, 1)
+
+    linear_transform = torch_nn.Linear(NUM_ITEMS, 1)
+    out = linear_transform(out)
+    print('out in forward', out.size())
+    return torch.transpose(out, 0, 1)
 
 def main():
   model = WordLSTM()
-  print('first input', inputs[0])
+  # print('first input', inputs[0])
   print('first input size', inputs[0].size())
   out = model(inputs[0])
-  print('hi')
-  print('out', out)
+  print('out in main', out.size())
 
 if __name__ == '__main__':
   main()
