ace4379f45c0bab445485961c5f01849089b181e
Adithya <adithya.bellary@gmail.com>
2021-08-31 13:51:19 -0500

    add link

--- midas/test_twilio.py
+++ midas/test_twilio.py
@@ -3,6 +3,6 @@ from services.notification_engine import Notification
 def main():
   notif_engine = Notification()
   notif_engine.send_message(message='testing 123')
-
+# https://www.twilio.com/docs/sms/tutorials/how-to-receive-and-reply-python
 if __name__ == '__main__':
   main()
