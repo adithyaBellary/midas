83d981c92950182adaec4a50317b9edc1b55f335
Adithya <adithya.bellary@gmail.com>
2021-09-03 23:30:04 -0500

    update env vars

--- services/notification_engine/engine.py
+++ services/notification_engine/engine.py
@@ -4,14 +4,14 @@ from django.conf import settings
 
 class Notification:
   def __init__(self):
-    self.TWILIO_SID = settings.TWILIO_SID
-    self.TWILIO_AUTH = settings.TWILIO_AUTH
+    self.TWILIO_ACCOUNT_SID = settings.TWILIO_ACCOUNT_SID
+    self.TWILIO_AUTH_TOKEN = settings.TWILIO_AUTH_TOKEN
     self.TWILIO_NUMBER = settings.TWILIO_NUMBER
     self.MY_NUMBER = settings.MY_NUMBER
 
     self.client = Client(
-      settings.TWILIO_SID,
-      settings.TWILIO_AUTH
+      settings.TWILIO_ACCOUNT_SID,
+      settings.TWILIO_AUTH_TOKEN
     )
 
   def send_message(self, message):
